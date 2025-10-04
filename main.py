f284eaff949d98ad8088847a6875315be8ba1076
Vincent W. <pc2025weingaertner@gmail.com>
2025-10-04 09:48:47 +0200

    feat: add image choice

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 print("Генератор мемов запущен!")
 top_text = input("Введи верхний текст: ")
 bottom_text = input("Введи нижний текст: ")
-print(f"{top_text}, {bottom_text}")
+
+list_of_images = ["1. Кот в ресторане", "2. Кот в очках"]
+print("Список картинок: ")
+for image_tittle in list_of_images:
+    print(image_tittle)
+
+user_image = input("Введите номер картинки: ")
+if user_image == "1":
+    image = "./images/Кот в очках.png"
+    print(image)
+elif user_image == "2":
+    image = "./images/Кот в ресторане.png"
+    print(image)
+
 
