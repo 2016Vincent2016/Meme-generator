b49ebda2b8786d354a17af42e5a51efb36777ac9
Vincent W. <pc2025weingaertner@gmail.com>
2025-10-06 17:08:03 +0200

    refactor: change images

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ print("Генератор мемов запущен!")
 top_text = input("Введи верхний текст: ")
 bottom_text = input("Введи нижний текст: ")
 
-list_of_images = ["1. Кот в ресторане", "2. Кот в очках"]
+list_of_images = ["1. Кошка в очках ", "2. Собака с пистолетом"]
 print("Список картинок: ")
 for image_tittle in list_of_images:
     print(image_tittle)
 
 user_image = input("Введите номер картинки: ")
 if user_image == "1":
-    image = "./images/Кот в очках.png"
+    image = "./images/Кошка в очках.png"
     print(image)
 elif user_image == "2":
-    image = "./images/Кот в ресторане.png"
+    image = "./images/Собака с пистолетом.png"
     print(image)
-
-
