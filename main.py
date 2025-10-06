e94c961aa4f69939332c01a64bf3bb2c79eeb39d
Vincent W. <pc2025weingaertner@gmail.com>
2025-10-06 17:54:51 +0200

    feat: generat text

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from PIL import Image, ImageDraw, ImageFont
+
 print("Генератор мемов запущен!")
 top_text = input("Введи верхний текст: ")
 bottom_text = input("Введи нижний текст: ")
@@ -9,8 +11,23 @@ for image_tittle in list_of_images:
 
 user_image = input("Введите номер картинки: ")
 if user_image == "1":
-    image = "./images/Кошка в очках.png"
-    print(image)
+    image_file = "./images/Кошка в очках.png"
+    print(image_file)
 elif user_image == "2":
-    image = "./images/Собака с пистолетом.png"
-    print(image)
+    image_file = "./images/Собака с пистолетом.png"
+    print(image_file)
+
+image = Image.open(image_file)
+draw = ImageDraw.Draw(image)
+
+google_font = "https://fonts.googleapis.com/css2?family=Luxurious+Script&display=swap"
+font = ImageFont.truetype("arial.ttf", size=25)
+
+draw.text((5, 0), top_text, font=font, fill="brown")
+draw.text((5, 50), bottom_text, font=font, fill="brown")
+
+image.save("new_image.png")
+
+
+
+
